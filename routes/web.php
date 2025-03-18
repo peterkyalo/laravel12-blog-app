a3446649ba8972d899df122acc1c77dd108d5728
peterkyalo <peterkyalo69@gmail.com>
2025-03-18 12:26:27 +0300

    Added Category model

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('home', 'home')->name('home');
 Route::view('about', 'about')->name('about');
